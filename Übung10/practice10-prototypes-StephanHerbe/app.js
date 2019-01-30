746f63bd9842e77c743ac69065aa0ee7f0d0a74c
StephanHerbe <44025495+StephanHerbe@users.noreply.github.com>
2019-01-30 14:28:17 +0100

    Add files via upload

---
+++ Übung10/practice10-prototypes-StephanHerbe/app.js
@@ -0,0 +1,10 @@
+let Square = require("./square.js");
+let Rectangle = require("./rectangle.js");
+
+let figures = [new Square(2), new Square(5), new Square(10), new Rectangle(5, 3), new Rectangle(10, 4)];
+
+for(let figure of figures) {
+    console.log(figure);
+    console.log(`Fläche: ${figure.getArea()}\nUmfang: ${figure.getCircumference()}`);
+    console.log();
+}
